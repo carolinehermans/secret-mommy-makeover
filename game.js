dcbfcdecc9b13908c680fd576ee781e87eab00e3
Caroline Hermans <carolinehermans@Carolines-Air.wv.cc.cmu.edu>
2017-12-07 17:39:18 -0500

    first commit

---
+++ game.js
@@ -0,0 +1,243 @@
+const makeupTypes = [
+  {
+    name: "letter",
+    funName: "...",
+    extraText: "mark? that's not daddy's name...",
+    extraImg: "letter-open",
+    noMark: true,
+    noCursor: true,
+    x: 190,
+    y: 380,
+    degrees: 165,
+    h: 90,
+    drawFn: () => {},
+  },
+  {
+    name: "blush",
+    funName: "a soft brush",
+    drawFn: () => {
+      tint(255, 40)
+      image(makeupTypes[1].empty, 0, 0)
+    },
+    x: 250,
+    y: 150,
+    degrees: -15,
+    h: 340,
+    t_off: -20,
+  },
+  {
+    name: "razor",
+    funName: "this one looks sharp",
+    markImgs: ["razor-mark1.png", "razor-mark2.png", "razor-mark3.png", "razor-mark4.png", "razor-mark5.png"],
+    noMark: true,
+    drawFn: () => {
+      tint(255, 255)
+      image(makeupTypes[2].empty, 0, 0)
+    },
+    x: 430,
+    y: 80,
+    degrees: -15,
+    h: 350,
+    t_off: 0,
+    w_off: 50,
+  },
+  {
+    name: "lipstick",
+    funName: "LIPSTICK! i'm gonna look so good",
+    drawFn: () => {
+      tint(255, 170)
+      image(makeupTypes[3].empty, 0, 0)
+    },
+    x: 650,
+    y: 290,
+    degrees: -3,
+    h: 200,
+    t_off: -7,
+  },
+  {
+    name: "condom",
+    funName: "lubricated... ooh, I bet it's moisturizer!",
+    drawFn: () => {
+      tint(255, 90)
+      image(makeupTypes[4].empty, 0, 0)
+    },
+    x: 630,
+    y: 90,
+    degrees: 7,
+    h: 160,
+    t_off: -40,
+  },
+  {
+    name: "eyeshadow",
+    funName: "smudgey dark stuff",
+    drawFn: () => {
+      tint(255, 80)
+      image(makeupTypes[5].empty, 0, 0)
+    },
+    x: 360,
+    y: 80,
+    degrees: 3,
+    w: 50,
+    h: 380,
+    t_off: -7,
+  },
+  // {
+  //   name: "eyeliner",
+  //   funName: "some kind of pencil",
+  //   drawFn: () => {
+  //     tint(255, 170)
+  //     image(makeupTypes[6].empty, 0, 0)
+  //   },
+  //   x: 480,
+  //   y: 120,
+  //   degrees: 20,
+  //   h: 370,
+  //   t_off: -2,
+  //   w_off: 5,
+  // },
+  {
+    name: "foundation",
+    funName: "looks like skin powder",
+    drawFn: () => {
+      tint(255, 100)
+      image(makeupTypes[6].empty, 0, 0)
+    },
+    x: 480,
+    y: 120,
+    degrees: 20,
+    h: 370,
+    t_off: -2,
+  },
+  {
+    name: "prozac",
+    funName: "maybe if I eat this, I can be happy like mommy",
+    extraText: "",
+    extraSound: "munch.wav",
+    extraImg: "empty",
+    noMark: true,
+    noCursor: true,
+    special: true,
+    drawFn: () => {},
+    x: 750,
+    y: 400,
+    degrees: -45,
+    h: 30,
+  },
+]
+
+makeupTypes.map((makeup, i) => {
+  makeup["img"] = makeup.name + ".png"
+  makeup["hoverImg"] = makeup.name + "-hover.png"
+  if (!makeup.noMark) makeup["markImg"] = makeup.name + "-mark.png"
+  if (!makeup.noCursor) makeup["cursorImg"] = makeup.name + "-cursor.png"
+})
+
+console.log(makeupTypes)
+currentState = "makeup"
+
+document.currentMakeup = makeupTypes[0]
+document.makeupChanged = false
+document.allMakeup = makeupTypes
+
+window.onload = () => {
+  let drawerButton = document.getElementById("drawer-button")
+
+  drawerButton.addEventListener("mouseenter", () => {
+    drawerButton.src = imgPath + "drawer-button-hover.png"
+  })
+
+  drawerButton.addEventListener("mouseleave", () => {
+    drawerButton.src = imgPath + "drawer-button.png"
+  })
+
+
+
+  makeupTypes.map((mkup, i) => {
+    let snd;
+    let parent = document.getElementById("drawer-state")
+    let makeupImg = document.createElement("img")
+    let mouseOverAudio = new Audio('pop.mp3')
+    if (mkup.extraSound) {
+      snd = new Audio(mkup.extraSound)
+    }
+    makeupImg.id = mkup.img
+    makeupImg.src = "./img/" + mkup.img
+    makeupImg.style.position = 'absolute'
+    makeupImg.style.left = mkup.x + 'px'
+    makeupImg.style.top = mkup.y + 'px'
+    makeupImg.className = "pointer"
+    makeupImg.style.transform = `rotate(${mkup.degrees}deg)`
+    makeupImg.style.height = `${mkup.h}px`
+    makeupImg.onclick = () => {
+      document.currentMakeup = makeupTypes[i]
+      makeupTypes[i].used = true
+      if (mkup.extraImg) {
+        document.getElementById(mkup.extraImg).style.display = "block"
+        makeupImg.style.display = "none"
+        document.getElementById("drawer-text").innerHTML = mkup.extraText
+        snd.play()
+      }
+      else {
+        changeState()
+      }
+    }
+    mkup['currentImg'] = mkup.img
+    makeupImg.addEventListener("mouseenter", () => {
+      makeupImg.src = imgPath + mkup.hoverImg
+      makeupImg.style.height = `${mkup.h + mkup.h/10}px`
+      makeupImg.style.top = mkup.y - mkup.h/20 + 'px'
+      makeupImg.style.left = mkup.x - mkup.h/50 + 'px'
+      mouseOverAudio.play();
+      document.getElementById('drawer-text').innerHTML = mkup.funName
+    })
+    makeupImg.addEventListener("mouseleave", () => {
+      makeupImg.src = imgPath + mkup.img
+      makeupImg.style.height = `${mkup.h}px`
+      makeupImg.style.top = mkup.y + 'px'
+      makeupImg.style.left = mkup.x + 'px'
+      if (document.getElementById('drawer-text').innerHTML != mkup.extraText) {
+        document.getElementById('drawer-text').innerHTML = ""
+      }
+    })
+    parent.appendChild(makeupImg)
+  })
+
+  stateCheck()
+}
+
+const states = ["intro", "makeup", "box", "end"]
+
+
+const changeState = () => {
+  if (currentState === "makeup") {
+    currentState = "drawer-state"
+  }
+  else if (currentState === "drawer-state") {
+    currentState = "makeup"
+    if (document.currentMakeup.cursorImg) {
+      document.currentMakeup.currentImg = document.currentMakeup.cursorImg
+    }
+  }
+  stateCheck();
+}
+
+const stateCheck = () => {
+  if (currentState === "makeup") {
+    document.makeupChanged = true
+    document.getElementById("drawer-state").style.display = "none"
+    document.getElementById("mirror").style.display = "block"
+    document.getElementById("empty-hover").style.display = "block"
+    document.getElementById("drawer-button").style.display = "block"
+    document.getElementById("done-button").style.display = "block"
+    document.getElementById("defaultCanvas0").style.display = "block"
+  }
+
+  if (currentState === "drawer-state") {
+    document.getElementById("defaultCanvas0").style.display = "none"
+    document.getElementById("mirror").style.display = "none"
+    document.getElementById("empty-hover").style.display = "none"
+    document.getElementById("drawer-button").style.display = "none"
+    document.getElementById("done-button").style.display = "none"
+    document.getElementById("drawer-state").style.display = "block"
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,104 @@
+let face;
+let mark;
+let cursor;
+let allEmpties;
+let clicked = false;
+let cursorX;
+let cursorY;
+let mirror;
+let makeups;
+let anim;
+let sound;
+let pastEmpties;
+let faceOverlay;
+let razorMakeup;
+const imgPath = "./img/"
+
+function preload() {
+  face = loadImage(imgPath + "face.png")
+  sound = loadSound('vibrate.mp3');
+  faceOverlay = loadImage(imgPath + "faceOverlay.png");
+  mark = loadImage(imgPath + "blush-mark.png")
+  cursor = loadImage(imgPath + "blush.png")
+  mirror = loadImage(imgPath + "mirror.png")
+
+  makeupTypes.map((makeup, i) => {
+    makeup['empty'] = loadImage(imgPath + "empty.png");
+    makeup['used'] = false;
+    if (!makeup.noCursor) makeup['cursorP5'] = loadImage(imgPath + makeup.cursorImg)
+    if (!makeup.noMark) makeup['markP5'] = loadImage(imgPath + makeup.markImg)
+
+    if (makeup.name === "razor") razorMakeup = makeup.markImgs.map((img, i) => loadImage(imgPath + img))
+  })
+  pastEmpties = loadImage(imgPath + "empty.png")
+}
+
+
+function mouseDragged() {
+  if (document.currentMakeup.img === "razor.png") {
+    return;
+  }
+
+  let m = document.currentMakeup.markP5
+
+  let w = m.width/2
+  let h = m.height/2
+  if (w < 0) w = 0
+  if (h < 0) h = 0
+
+  document.currentMakeup.empty.blend(m, 0, 0, m.width, m.height, mouseX - w, mouseY - h, m.width, m.height, DARKEST)
+}
+
+function mousePressed() {
+  if (document.currentMakeup.img === "razor.png") {
+    clicked = true;
+    let w = mark.width/2
+    let h = mark.height/2
+    if (w < 0) w = 0
+    if (h < 0) h = 0
+    let i = Math.floor(Math.random() * document.currentMakeup.markImgs.length)
+    document.currentMakeup.empty.blend(razorMakeup[i], 0, 0, razorMakeup[i].width, razorMakeup[i].height, mouseX - w, mouseY - h, razorMakeup[i].width, razorMakeup[i].height, DARKEST)
+  }
+}
+
+
+function setup() {
+  canvas = createCanvas(980, 620);
+  background(255)
+}
+
+function doneDraw() {
+  document.getElementById("mirror").style.display = "none";
+  document.getElementById("wash").style.display = "block";
+  document.getElementById("drawer-button").style.display = "none";
+  document.getElementById("done-button").style.display = "none";
+  document.getElementById("restart-button").style.display = "block";
+}
+
+function draw() {
+
+  if (document.done) {
+    doneDraw();
+    return;
+  }
+
+  if (currentState != "makeup") return;
+
+  image(face, 160, 15);
+
+  makeupTypes.map((makeup, i) => {
+    if (makeup.used)
+      makeup.drawFn();
+  })
+
+  tint(255, 255);
+
+  image(faceOverlay, 160, 15);
+
+  let off_w = 0;
+  if (document.currentMakeup.w_off) off_w = document.currentMakeup.w_off;
+
+  if (mouseY < 520 && document.currentMakeup.cursorP5) {
+    image(document.currentMakeup.cursorP5, mouseX - document.currentMakeup.cursorP5.width / 2 + off_w, mouseY + document.currentMakeup.t_off);
+  }
+}
